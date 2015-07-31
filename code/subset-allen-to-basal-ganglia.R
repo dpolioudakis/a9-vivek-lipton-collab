e53b27099c7a20c2badd6eba61078d6a3e6517aa
Damon Polioudakis <damon.polioudakis@gmail.com>
2015-07-30 17:32:00 -0700

    tweak to specify basal ganglia subset and clean code

--- code/subset-allen-to-basal-ganglia.R
+++ code/subset-allen-to-basal-ganglia.R
@@ -7,14 +7,14 @@ print("Starting subset-allen-to-basal-ganglia.R script...")
 print("Loading Allen Brain microarray data...")
 
 #Functions to load data
-load.array.data <- function (bio.rep.folder) {
+Load_array_data <- function (bio.rep.folder) {
      array.data.path <- paste(
           "../raw_data/allen_data/", bio.rep.folder, "/MicroarrayExpression.csv"
           , sep= ""
      )
      read.csv(array.data.path, header=FALSE)
 }
-load.meta.data <- function (bio.rep.folder) {
+Load_meta_data <- function (bio.rep.folder) {
      meta.data.path <- paste(
           "../raw_data/allen_data/", bio.rep.folder, "/SampleAnnot.csv"
           , sep= ""
@@ -35,8 +35,8 @@ array.data.folder.names <- list(
 #Load the Allen Brain microarray data
 #Columns correspond to samples and samples are listed in the same order in the
 #meta and array data
-exp.array.data <- lapply(array.data.folder.names, load.array.data)
-meta.array.data <- lapply(array.data.folder.names, load.meta.data)
+exp.array.data <- lapply(array.data.folder.names, Load_array_data)
+meta.array.data <- lapply(array.data.folder.names, Load_meta_data)
 
 # save(array.data, file="../processed.data/all.allen.array.data.rda")
 ################################################################################
@@ -45,33 +45,39 @@ meta.array.data <- lapply(array.data.folder.names, load.meta.data)
 print("Subsetting data to select the substantia nigra samples...")
 
 # Function to subset the meta data
-subset.sn.meta.data.fxn <- function (bio.rep.meta.data) {
-     subset(bio.rep.meta.data, grepl("substantia nigra", structure_name))
+Subset_meta_data <- function (bio.rep.meta.data, to.match) {
+     subset(bio.rep.meta.data, grepl(paste(to.match, collapse="|"), structure_name))
 }
 
+# Areas to subset, identified by unix grep command:
+     # grep -r --include SampleAnnot* 'accumbens\|\caudate\|putamen\
+     # |substantia\|subthalamic' 17* | cut -d, -f4,5,6,7 | sort -u | column
+areas.to.subset <- c("substantia nigra", "nucleus accumbens", "caudate"
+                    ,"putamen", "subthalamic")
+
 #Function to subset the array expression data
-subset.array.data.fxn <- function (bio.rep.exp.array.data, bio.rep.meta.data) {
+Subset_array_data <- function (bio.rep.exp.array.data, bio.rep.meta.data) {
      subset.array.data <- bio.rep.exp.array.data[ , grepl(
           "substantia nigra", bio.rep.meta.data$structure_name)]
 }
 
 #Subset the meta data
-subset.sn.meta.data <- lapply(meta.array.data, subset.sn.meta.data.fxn)
-#Meta data substantia nigra number of samples and number of features
-print("Meta data substantia nigra number of samples and number of features:")
-sapply(subset.sn.meta.data, dim)
-#Total number of substantia nigra samples
-print("Total number of substantia nigra samples:")
-sum(sapply(subset.sn.meta.data, dim)[1, ])
+meta.data.subset <- lapply(meta.array.data, Subset_meta_data, areas.to.subset)
+#Meta data number of samples and number of features
+print("Meta data subset number of samples (Row1) and number of features (Row2):")
+sapply(meta.data.subset, dim)
+print("Total number of samples in subset:")
+sum(sapply(meta.data.subset, dim)[1, ])
 
 #Subset the array expression data
-subset.sn.array.data <- do.call(cbind,
-                                mapply(subset.array.data.fxn, exp.array.data, meta.array.data))
+array.data.subset <- do.call(cbind, mapply(Subset_array_data
+                                           , exp.array.data
+                                           , meta.array.data))
 #Add the probe ID numbers as column 1
-subset.sn.array.data <- cbind(probe=exp.array.data[[1]][,1], subset.sn.array.data)
+array.data.subset <- cbind(probe=exp.array.data[[1]][,1], array.data.subset)
 print("Dimensions of subset expression data, columns are samples:")
 print("(row 1 is Probe ID)")
-dim(subset.sn.array.data)
+dim(array.data.subset)
 
-save(subset.sn.array.data, subset.sn.meta.data,
-     file="../processed_data/subset.sn.array.data.rda")
+save(array.data.subset, meta.data.subset,
+     file="../processed_data/array.data.subset.rda")
