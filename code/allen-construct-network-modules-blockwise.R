9e931e8c000b7ca83a17954cf44632d3486fc8cc
Damon Polioudakis UCLA mbp <damon.polioudakis@gmail.com>
2015-08-14 17:24:03 -0700

    add tests of ME merge parameter and graph correlations

--- code/allen-construct-network-modules-blockwise.R
+++ code/allen-construct-network-modules-blockwise.R
@@ -3,6 +3,14 @@
 # dendrograms of the separate blocks?
 
 
+# Workflow
+#   1a-allen-subset-to-basal-ganglia.R
+#   1b-allen-combine-probes.R
+#   2a-allen-soft-thresholding-power.R
+#   2b-allen-adjacency-TOM.R
+#   3-allen-construct-network-modules.R
+#   4-allen-compare-modules-metadata.R
+
 
 # Cluster samples and construct modules blockwise
 
@@ -38,7 +46,7 @@ softPower = 5
 MakeBWModules <- function (minModSize, deepSplit, mergeCutHeight) {
   print("Treecut arguments:")
   print(c(minModSize, deepSplit, mergeCutHeight))
-  bwNet = blockwiseModules(exprData, maxBlockSize = 10000, power = softPower
+  bwNet = blockwiseModules(exprData, maxBlockSize = 12000, power = softPower
                            , TOMType = "signed", minModuleSize = minModSize
                            , reassignThreshold = 0, pamStage = FALSE,
                            , numericLabels = TRUE, deepSplit = deepSplit
@@ -53,8 +61,34 @@ MakeBWModules <- function (minModSize, deepSplit, mergeCutHeight) {
 minModSize <- c(30)
 deepSplit <- c(2)
 # Cut height of 0.25, corresponds to a correlation of 0.75, to merge ME:
-MEmergeCutHeight <- c(0.2)
-bwModules <- MakeBWModules(minModSize, deepSplit, MEmergeCutHeight)
+MEmergeCutHeight <- c(0.2, 0.25, 0.3)
+# bwModules <- MakeBWModules(minModSize, deepSplit, MEmergeCutHeight)
+# blockwiseMEs <- moduleEigengenes(exprData, bwModules$colors)$eigengenes
+
+bwModules <- mapply(MakeBWModules
+                    , MEmergeCutHeight
+                    , MoreArgs = list(
+                      minModSize = minModSize, deepSplit = deepSplit)
+                    , SIMPLIFY = FALSE)
+
+blockwiseMEs <- lapply(bwModules, function (x)
+                       moduleEigengenes(exprData, x$colors)$eigengenes)
+
+
+# signif(cor(blockwiseMEs, blockwiseMEs), 3)
+
+pdf("../analysis/3a_BW_ME_correlation.pdf", height=8, width=8)
+par(cex = 1.0)
+# sizeGrWindow(6,7)
+par(mfrow = c(3,1))
+for (i in 1:length(blockwiseMEs)) {
+  plotEigengeneNetworks(orderMEs(blockwiseMEs[[i]])
+    , paste("Eigengene correlation - merge cut height:", MEmergeCutHeight[i])
+    , signed=TRUE, colorLabels=TRUE,
+    , marHeatmap = c(1,4,1,2)
+    , marDendro = c(0,4,2,0))
+}
+dev.off()
 
 save(bwModules, exprData, file="../processed_data/allen_BW_modules.rda")
 
