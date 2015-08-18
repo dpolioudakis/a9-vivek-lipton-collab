c5699847167e2770f99ea8aed2d6c7b893bd19ac
Damon Polioudakis UCLA mbp <damon.polioudakis@gmail.com>
2015-08-18 16:26:02 -0700

    update variable bwModulesLL name from input

--- code/allen-A9-marker-expr-in-hESC-A9.R
+++ code/allen-A9-marker-expr-in-hESC-A9.R
@@ -5,6 +5,8 @@ print("Starting allen-A9-marker-expr-in-hESC-A9.R script...")
 sessionInfo()
 
 library(WGCNA)
+library(reshape2)
+library(ggplot2)
 
 options(stringsAsFactors=FALSE)
 allowWGCNAThreads()
@@ -12,14 +14,13 @@ disableWGCNAThreads()
 
 load("../processed_data/allen_BW_modules.rda")
 load("../processed_data/array_data_subset_avg_probes.rda")
+# Vivek normalized RNAseq FPKMs from hESC derived A9 neuronal cultures
+load("../HTSeqUnion_Exon_CQN_OutlierRemoved_A9cells.rda")
 
 # bwModules is list of modules from 3 different ME merge cut heights
 blockwiseMEs <- moduleEigengenes(exprData, bwModules[[3]]$colors)$eigengenes
 
-
-# Expression of A9 module 28 genes in hESC derived A9 neuronal cultures
-
-
+# Write table of gene names in module 28 (ME correlated with A9 markers)
 geneModuleMembership <- as.data.frame(cor(exprData, blockwiseMEs, use = "p"))
 module=28
 moduleGenes <- bwModules[[3]]$colors==module
@@ -28,48 +29,8 @@ row.names(geneModuleMembership[moduleGenes, ])
 module28Genes <- data.frame(row.names(geneModuleMembership[moduleGenes, ]))
 write.table(module28Genes, "../analysis/SN_module_genes.txt")
 
-
-
-library(reshape2)
-library(ggplot2)
-
-setwd(".")
-load("../HTSeqUnion_Exon_CQN_OutlierRemoved_A9cells.rda")
-```
-
-Data frame of Ensembl IDs, gene symbols, and status as marker or anti-marker
-```{r marker_ensembl_labels}
-markers.df <- data.frame(
-  ensembl=
-    c(
-      "ENSG00000180176",
-      "ENSG00000157542",
-      "ENSG00000165646",
-      "ENSG00000157388",
-      "ENSG00000162761",
-      "ENSG00000125798",
-      "ENSG00000153234",
-      "ENSG00000165092",
-      
-      "ENSG00000104327",
-      "ENSG00000172137",
-      "ENSG00000165588",
-      "ENSG00000165655",
-      "ENSG00000148680"
-    ),
-  gene=
-    c(
-      #markers
-      "TH", "KCNJ7","VMAT2","CACNA1D","LMX1A","FOXA2","NURR1","ALDH1",
-      #anti markers
-      "CALB1", "CALB2", "OTX2", "NOLZ1", "HTR7"
-      #"NOLZ1"(also known as ZNF503)
-    ),
-  marker.type=
-    c(rep("marker",8),rep("anti-marker",5))
-)
-```
-
+# Data frame of Ensembl IDs, gene symbols, and status as marker or anti-marker
+# Start by making list of Enzembl IDs and gene symbols
 module28Ensembl <- c("ENSG00000165092"=	"ALDH1A1"
                      ,"ENSG00000164512"=	"ANKRD55"
                      ,"ENSG00000186897"=	"C1QL4"
@@ -111,32 +72,32 @@ module28Ensembl <- c("ENSG00000165092"=	"ALDH1A1"
                      ,"ENSG00000180176"=	"TH"
                      ,"ENSG00000182223"=	"ZAR1")
 
+# Convert to data frame
+module28EnsemblDF <- data.frame(names(module28Ensembl)
+                                , module28Ensembl, row.names = NULL)
+
+# Subset Vivek produced expression data by marker genes present
+markersExprDF <- datExpr.HTSC.A9[
+  (rownames(datExpr.HTSC.A9) %in% module28EnsemblDF[,1]), ]
+
+# Combine data frames to add gene symbol and marker information
+markersExprDF <- merge(markersExprDF, module28EnsemblDF
+                       , by.x="row.names", by.y=1)
+# Changing column name like this is not working
+colnames(markersExprDF[1]) <- "ensembl"
 
 
-module28EnsemblDF <- data.frame(names(module28Ensembl), module28Ensembl, row.names = NULL)
+# Reshape for ggplot2 using Reshape2 package
+markersExprDF <- melt(markersExprDF
+                      , value.name="expression", variable.name="sample")
 
-Subset expression data by marker genes present
-```{r subset_markers}
-markers.exp.df <- datExpr.HTSC.A9[(rownames(datExpr.HTSC.A9) %in% module28EnsemblDF[,1]),]
-```
-Combine data frames to add gene symbol and marker information
-```{r combine_marker_expression_dfs}
-markers.exp.df <- merge(markers.exp.df, module28EnsemblDF, by.x="row.names", by.y=1)
-colnames(markers.exp.df[1]) <- "ensembl"
-colnames(markers.exp.df[7])
-```
-Reshape for ggplot2 using Reshape2 package
-```{r reshape_marker_expression}
-markers.exp.df <- melt(markers.exp.df, value.name="expression", variable.name="sample")
-```
-Add column with biological replicate label
-```{r add_bio_rep_label}
-markers.exp.df <- cbind(markers.exp.df, bio.rep = as.factor(c(rep(2,36), rep(7,36))))
-```
+# Add column with biological replicate label
+markersExprDF <- cbind(markersExprDF
+                       , bio.rep = as.factor(c(rep(2,36), rep(7,36))))
 
-Plot marker genes expression
-```{r plot_markers}
-ggplot(markers.exp.df, aes(x=module28Ensembl, y=expression)) + 
+
+# Plot expression marker genes high MEF2C and low MEF2C samples
+ggplot(markersExprDF, aes(x=module28Ensembl, y=expression)) + 
   geom_boxplot(aes(fill=bio.rep)) +
   scale_fill_discrete(name= "Biological\nReplicate",
                       labels= c("2_(high MEF2C)", "7_(low MEF2C)")) +
@@ -145,4 +106,3 @@ ggplot(markers.exp.df, aes(x=module28Ensembl, y=expression)) +
   xlab("Gene") +
   theme_grey(base_size = 18)
 ggsave(file= "A9 marker gene expression.pdf")
-```
