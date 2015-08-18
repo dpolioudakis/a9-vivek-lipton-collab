5e8eb12b4b2cdb8867da23f24ea6c7d0dc6df73c
Damon Polioudakis UCLA mbp <damon.polioudakis@gmail.com>
2015-08-18 16:24:36 -0700

    add testing of multiple module construction parameters

--- code/allen-construct-network-modules-blockwise.R
+++ code/allen-construct-network-modules-blockwise.R
@@ -24,7 +24,20 @@ library(flashClust)
 
 options(stringsAsFactors=FALSE)
 allowWGCNAThreads()
-disableWGCNAThreads() 
+disableWGCNAThreads()
+
+softPower = c(5,7,9)
+minModSize <- c(30, 50, 100)
+deepSplit <- c(2)
+# Cut height of 0.25, corresponds to a correlation of 0.75, to merge ME:
+MEmergeCutHeight <- c(0.2, 0.25, 0.3)
+maxBlockSize <- c(300) #12000
+ArgsbwModulesLLDF <- expand.grid(softPower = softPower
+                             , minModSize = minModSize
+                             , deepSplit = deepSplit
+                             , MEmergeCutHeight = MEmergeCutHeight
+                             , maxBlockSize = maxBlockSize)
+
 
 load("../processed_data/array_data_subset_avg_probes.rda")
 
@@ -37,46 +50,58 @@ exprData <- t(arrayDataSubsetAvgProbesDF)
 # exprDataRandom5000 <- exprData[ , sample(ncol(exprData), 5000)]
 # exprData <- exprDataRandom5000
 
-softPower = 5
+
 
 # Module identification using blockwise:
 
 # Function to construct modules
+# blockwiseModules identifies modules using Dynamic Hybrid tree cut
 # Args: minimum module size, deep split
-MakeBWModules <- function (minModSize, deepSplit, mergeCutHeight) {
-  print("Treecut arguments:")
-  print(c(minModSize, deepSplit, mergeCutHeight))
-  bwNet = blockwiseModules(exprData, maxBlockSize = 12000, power = softPower
-                           , TOMType = "signed", minModuleSize = minModSize
-                           , reassignThreshold = 0, pamStage = FALSE,
-                           , numericLabels = TRUE, deepSplit = deepSplit
-                           , mergeCutHeight = mergeCutHeight
-                           ,  verbose = 3)
+MakebwModulesLL <- function (softPower
+                           , minModSize
+                           , deepSplit
+                           , MEmergeCutHeight
+                           , maxBlockSize) {
+  print("WGCNA blockwiseModules function arguments:")
+  print(c(softPower, minModSize, deepSplit, MEmergeCutHeight, maxBlockSize))
+  bwNet <- blockwiseModules(  exprData
+                           , power = softPower
+                           , minModuleSize = minModSize
+                           , deepSplit = deepSplit
+                           , mergeCutHeight = MEmergeCutHeight
+                           , maxBlockSize = maxBlockSize
+                           , TOMType = "signed"
+                           , reassignThreshold = 0, pamStage = FALSE
+                           , numericLabels = TRUE
+                           , verbose = 3)
   print("Table of genes per module:")
   print(table(bwNet$colors))
   bwNet
 }
 
+# ArgsbwModulesLLDF is all combinations of arguments for MakebwModulesLL
+# Each row of ArgsbwModulesLLDF is a set of arguments
+# bwModuleLL is a list, each item is an output of MakebwModulesLL that is
+# calling the WGCNA blockwiseModules function.  blockwiseModules output is a
+# list
+bwModulesLL <- apply(ArgsbwModulesLLDF, 1
+                     , function(x) MakebwModulesLL(x[1],x[2],x[3],x[4],x[5]))
 
-minModSize <- c(30)
-deepSplit <- c(2)
-# Cut height of 0.25, corresponds to a correlation of 0.75, to merge ME:
-MEmergeCutHeight <- c(0.2, 0.25, 0.3)
-# bwModules <- MakeBWModules(minModSize, deepSplit, MEmergeCutHeight)
-# blockwiseMEs <- moduleEigengenes(exprData, bwModules$colors)$eigengenes
-
-bwModules <- mapply(MakeBWModules
-                    , MEmergeCutHeight
-                    , MoreArgs = list(
-                      minModSize = minModSize, deepSplit = deepSplit)
-                    , SIMPLIFY = FALSE)
-
-blockwiseMEs <- lapply(bwModules, function (x)
-                       moduleEigengenes(exprData, x$colors)$eigengenes)
+# DELETE THIS BLOCK IF SCRIPT UPDATE WORKS
+# # bwModulesLL <- MakebwModulesLL(minModSize, deepSplit, MEmergeCutHeight)
+# # blockwiseMEs <- moduleEigengenes(exprData, bwModulesLL$colors)$eigengenes
+# bwModulesLL <- mapply(MakebwModulesLL
+#                     , MEmergeCutHeight
+#                     , MoreArgs = list(
+#                       minModSize = minModSize, deepSplit = deepSplit)
+#                     , SIMPLIFY = FALSE)
 
 
-# signif(cor(blockwiseMEs, blockwiseMEs), 3)
 
+# Graph module eigengene correlations for each set of arguments used in
+# blockwiseModules WGCNA function
+blockwiseMEs <- lapply(bwModulesLL, function (x)
+  moduleEigengenes(exprData, x$colors)$eigengenes)
 pdf("../analysis/3a_BW_ME_correlation.pdf", height=8, width=8)
 par(cex = 1.0)
 # sizeGrWindow(6,7)
@@ -90,6 +115,6 @@ for (i in 1:length(blockwiseMEs)) {
 }
 dev.off()
 
-save(bwModules, exprData, file="../processed_data/allen_BW_modules.rda")
+save(bwModulesLL, exprData, file="../processed_data/allen_BW_modules.rda")
 
 
