7abc914c2b5edddff7d6965cec2ffdcee73d8d5d
Damon Polioudakis UCLA mbp <damon.polioudakis@gmail.com>
2015-08-20 13:41:04 -0700

    add loop to plot all module groups

--- code/allen-ME-expr-corr-A9-markers.R
+++ code/allen-ME-expr-corr-A9-markers.R
@@ -1,3 +1,6 @@
+# Correlate A9 marker genes to all module groups produced by each run of WGCNA
+# blockwise module function with different parameters
+
 print("#######################################################################")
 print("Starting allen-ME-expr-corr-A9-markers.R script...")
 sessionInfo()
@@ -11,29 +14,33 @@ disableWGCNAThreads()
 load("../processed_data/allen_BW_modules.rda")
 load("../processed_data/array_data_subset_avg_probes.rda")
 
-# bwModules is list of modules from 3 different ME merge cut heights
-blockwiseMEs <- moduleEigengenes(exprData, bwModules[[3]]$colors)$eigengenes
+# bwModulesLL is list of modules from different blockwiseModules parameters used
 
 # Correlate ME expression for each sample to marker gene expression
 # CALB1 and CALB2 are anti-markers
-markerMEcor <- cor(bwModules[[3]]$MEs, t(arrayDataSubsetAvgProbesDF[
-  c("ALDH1A1", "TH", "SLC18A2", "KCNJ6", "CALB1"), ]))
+markerMEcor <- lapply(bwModulesLL, function(x)
+  cor(x$MEs, t(arrayDataSubsetAvgProbesDF[
+  c("ALDH1A1", "TH", "SLC18A2", "KCNJ6", "CALB1"), ])))
 
 # Correlate with all markers listed in Vivek's work
-markerMEcor <- cor(bwModules[[3]]$MEs, t(arrayDataSubsetAvgProbesDF[
+markerMEcor <- lapply(bwModulesLL, function(x)
+  cor(x$MEs, t(arrayDataSubsetAvgProbesDF[
   c("ALDH1A1", "TH", "SLC18A2", "CACNA1D", "CALB1"
-    , "CALB2", "KCNJ6", "LMX1A", "FOXA2", "NR4A2", "ALDH1A1"), ]))
+    , "CALB2", "KCNJ6", "LMX1A", "FOXA2", "NR4A2", "ALDH1A1"), ])))
 
-pdf("../analysis/3b_MEs_markers_corr.pdf", height=8, width=8)
-labeledHeatmap(markerMEcor
-               , colnames(markerMEcor)
-               , rownames(markerMEcor)
-               , colorLabels = FALSE
-               , colors=greenWhiteRed(50)
-               , setStdMargins = TRUE
-               , textMatrix = signif(markerMEcor,2)
-               , cex.text = 1
-               , cex.lab = 1
-               , zlim = c(-1,1)
-               , main = "Gene to module eigenegene correlation")
+pdf("../analysis/3b_MEs_markers_corr-2015-08-20.pdf", height=8, width=8)
+for (i in 1:length(blockwiseMEs)) {
+  labeledHeatmap(markerMEcor[[i]]
+                 , colnames(markerMEcor[[i]])
+                 , rownames(markerMEcor[[i]])
+                 , colorLabels = FALSE
+                 , colors=greenWhiteRed(50)
+                 , setStdMargins = TRUE
+                 , textMatrix = signif(markerMEcor[[i]],2)
+                 , cex.text = 1
+                 , cex.lab = 1
+                 , zlim = c(-1,1)
+                 , main = paste(colnames(ArgsbwModulesLLDF)
+                                , c(ArgsbwModulesLLDF[i,]), collapse=" "))
+}
 dev.off()
