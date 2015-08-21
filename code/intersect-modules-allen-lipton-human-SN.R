0c202bbd6c9e0916a4362759bc4731ea129ea813
Damon Polioudakis UCLA mbp <damon.polioudakis@gmail.com>
2015-08-21 16:08:22 -0700

    finish and test pbinom calculation section

--- code/intersect-modules-allen-lipton-human-SN.R
+++ code/intersect-modules-allen-lipton-human-SN.R
@@ -8,92 +8,72 @@ load("../Vivek_WGCNA_Lipton_A9_SN/Vivek_SN_mod_colors.rda")
 load("../processed_data/allen_BW_modules.rda")
 
 # Split by model into lists or dataframes of genes in each model
-allenGenesModels <- split(colnames(exprData), bwModulesLL[[3]]$colors)
-humanSNgenesModels <- split(geneInfo[,1:2], geneInfo$`Initially Assigned Module Color`)
+allenGenesModels <- split(colnames(exprData), bwModulesLL[[18]]$colors)
+humanSNgenesModelsLL <- split(geneInfo[,1:2]
+                              , geneInfo$`Initially Assigned Module Color`)
 # Dataframe of all combinations of models from Allen and Lipton human Substantia
 # Nigra
-modelCombos <- expand.grid(names(allenGenesModels), names(humanSNgenesModels))
+modelCombos <- expand.grid(names(allenGenesModels), names(humanSNgenesModelsLL))
 
 # Intersection genes for every combination of models
 allenHumanSNintersect <- apply(modelCombos, 1
   , function(x) length(intersect(allenGenesModels[[x[1]]]
-                                 , humanSNgenesModels[[x[2]]])))
+                                 , humanSNgenesModelsLL[[x[2]]])))
 # Add combination of models labels
 allenHumanSNintersect <- cbind(modelCombos, allenHumanSNintersect)
 # Reshape into matrix
 allenHumanSNintersect <- dcast(allenHumanSNintersect
                                , Var1~Var2, value.var="allenHumanSNintersect")
 
-# Filter for intersection of genes in Allen and Lipton human substantia nigra
+# Filter for intersection of all genes in Allen and Lipton human substantia nigra
 totalIntersectGenesProfiled <- intersect(geneInfo$GeneSymbol
                                          , colnames(exprData))
-# Filter each model
+# Filter each model in Allen and Lipton for all genes that intersect
 allenGenesModels <- lapply(allenGenesModels, function(x)
                              intersect(x, totalIntersectGenesProfiled))
-humanSNgenesModels <- lapply(humanSNgenesModels, function(x)
+humanSNgenesModelsLL <- lapply(humanSNgenesModelsLL, function(x)
   intersect(x$GeneSymbol, totalIntersectGenesProfiled))
 
 # Intersection genes for every combination of models
 allenHumanSNintersect <- apply(modelCombos, 1, function(x)
-  length(intersect(allenGenesModels[[x[1]]], humanSNgenesModels[[x[2]]])))
+  length(intersect(allenGenesModels[[x[1]]], humanSNgenesModelsLL[[x[2]]])))
 allenHumanSNintersect <- cbind(modelCombos, allenHumanSNintersect)
 allenHumanSNintersect <- dcast(allenHumanSNintersect
                                , Var1~Var2, value.var="allenHumanSNintersect")
 
-phyper(q=3, m=12, n=7147-12, k=211, lower.tail=FALSE, log.p=FALSE)
-length(allenGenesModels[[29]])
-length(humanSNgenesModels$skyblue)
-length(totalIntersectGenesProfiled)
-
-sapply(allenHumanSNintersect[29,], function(x)
-         phyper(
-           q=(x-1) #size of overlap - 1
-         , m=length(allenGenesModels[[29]]) # genes in Allen module
-         # total number of genes shared between Lipton and Allen - genes in
-         # Lipton human SN model
-         , n=length(totalIntersectGenesProfiled)-length(humanSNgenesModels$skyblue)
-         , k=length(humanSNgenesModels$skyblue)  # genes in Lipton human SN model
-         , lower.tail=FALSE, log.p=FALSE))
-
-pvals <- apply(allenHumanSNintersect, 1, function(x)
-  #print(length(allenGenesModels[[x[1]]])))
-  phyper(
-    q=as.numeric(x[3])-1 #size of overlap - 1
-    , m=length(allenGenesModels[[x[1]]]) # genes in Allen module
-    # total number of genes shared between Lipton and Allen - genes in
-    # Lipton human SN model
-    , n=length(totalIntersectGenesProfiled)-length(humanSNgenesModels[[x[2]]])
-    , k=length(humanSNgenesModels[[x[2]]])  # genes in Lipton human SN model
-    , lower.tail=FALSE, log.p=FALSE))
-pvals <- sapply(pvals, function(x) round(x,2))
-
-allenHumanSNintersectPvals <- cbind(modelCombos, pvals)
-allenHumanSNintersectPvals <- dcast(allenHumanSNintersectPvals
-                               , Var1~Var2, value.var="pvals")
-allenHumanSNintersectPvals <- data.matrix(allenHumanSNintersectPvals)
-rownames(allenHumanSNintersectPvals) <- allenHumanSNintersectPvals[ ,1]
-allenHumanSNintersectPvals <- allenHumanSNintersectPvals[ ,-1]
-
-
-allenHumanSNintersect[29,]
-allenHumanSNintersect$skyblue
-
-allenHumanSNintersect
-data.class(allenHumanSNintersect)
-allenHumanSNintersect <- data.matrix(allenHumanSNintersect)
-data.class(allenHumanSNintersect[1,1])
-
-labeledHeatmap(allenHumanSNintersect, xLabels=NULL)
-labeledHeatmap(allenHumanSNintersectPvals, xLabels=NULL)
-
-labeledHeatmap(markerMEcor
-               , colnames(markerMEcor)
-               , rownames(markerMEcor)
-               , colorLabels = FALSE
-               , colors=greenWhiteRed(50)
+# Move row names from column 1 to row names
+rownames(allenHumanSNintersect) <- allenHumanSNintersect[ ,1]
+allenHumanSNintersect <- allenHumanSNintersect[ ,-1]
+# Initialize empty matrix to hold pvalues
+pVals <- matrix( , nrow = nrow(allenHumanSNintersect)
+                 , ncol = ncol(allenHumanSNintersect)
+                 , dimnames = list(rownames(allenHumanSNintersect)
+                                   , colnames(allenHumanSNintersect)))
+# Calc pbinomal of interesection for each model in Lipton human substantia nigra
+# with each model in Allen
+for (j in 1:(ncol(allenHumanSNintersect))) {
+  for (i in 1:nrow(allenHumanSNintersect)) {
+    pVals[i,j] <- phyper(
+        q = (as.numeric(allenHumanSNintersect[i,j])-1)
+      , m = length(allenGenesModels[[i]]) # genes in Allen module
+      , n = length(totalIntersectGenesProfiled)-length(humanSNgenesModelsLL[[j]])
+      , k = length(humanSNgenesModelsLL[[j]])
+      , lower.tail = FALSE, log.p = FALSE)
+  }
+}
+# Format number of genes that intersect with respective pbinom
+textMatrix <- paste(as.matrix(allenHumanSNintersect), "\n", round(pVals,3), sep = "")
+# Plot pbinoms with number of genes that intersect included on label
+pdf(paste("../analysis/intersection_Allen_human_SN-minModSize100-", Sys.Date(), ".pdf"
+          , sep = ""), height=8, width=8)
+labeledHeatmap(pVals
+               , yLabels = rownames(pVals)
+               , xLabels = colnames(pVals)
+               , textMatrix = textMatrix
                , setStdMargins = TRUE
-               , textMatrix = signif(markerMEcor,2)
-               , cex.text = 1
+               , cex.text = 0.5
                , cex.lab = 1
-               , zlim = c(-1,1)
-               , main = "Gene to module eigenegene correlation")
+               , zlim = c(0,1)
+               , main = "pbinom Lipton Human Substantia nigra and Allen"
+               )
+dev.off()
