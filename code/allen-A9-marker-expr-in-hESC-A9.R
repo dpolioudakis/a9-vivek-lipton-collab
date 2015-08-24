8ef3fd2f4aa66e4ea5bd45c3dbf3d7025c0f4b99
Damon Polioudakis UCLA mbp <damon.polioudakis@gmail.com>
2015-08-23 22:30:51 -0700

    finish PCA graph, add expression ratio boxplot

--- code/allen-A9-marker-expr-in-hESC-A9.R
+++ code/allen-A9-marker-expr-in-hESC-A9.R
@@ -22,10 +22,12 @@ load("../Vivek_WGCNA_Lipton_A9_SN/HTSeqUnion_Exon_CQN_OutlierRemoved_A9cells.rda
 # 12 corresponds to softPower 9, minModSize 30, deepSplit 2,
 # MEmergeCutHeight 0.25, maxBlockSize 12000
 modNetworkToUse <- 12
-modsToUse <- c("saddlebrown", "salmon", "red", "pink", "black", "green", "grey60")
+modsToUse <- c("saddlebrown", "salmon", "red", "pink", "black", "green"
+               , "grey60")
 
 modNetworkToUse <- 11
-modsToUse <- c("plum1", "grey60"), "brown", "cyan", "green", "sienna3", "royalblue")
+modsToUse <- c("plum1", "grey60", "brown", "red", "cyan", "yellowgreen"
+               , "sienna3", "royalblue")
 
 modNetworkToUse <- 18
 modsToUse <- c("salmon", "blue", "purple")
@@ -35,11 +37,10 @@ SelectModule <- function (modNetworkToUse, modToUse) {
   blockwiseMEs <- moduleEigengenes(
     exprData, bwModulesLL[[modNetworkToUse]]$colors)$eigengenes
   
-  # Write table of gene names in module 28 (ME correlated with A9 markers)
+  # Write table of gene names in module - is correlation even necessary?
+  # Pretty sure I copied this from WGCNA tutorial?
   geneModuleMembership <- as.data.frame(cor(exprData, blockwiseMEs, use = "p"))
   moduleGenes <- bwModulesLL[[modNetworkToUse]]$colors==modToUse
-  # Test code to check with random sample of genes:
-#   moduleGenes <- c(sample(1:length(bwModulesLL[[modNetworkToUse]]$colors), 20000, replace=F))
   moduleGenes <- data.frame(row.names(geneModuleMembership[moduleGenes, ]))
   # bioMart manual:
   # http://bioconductor.org/packages/release/bioc/vignettes/biomaRt/inst/doc/biomaRt.pdf
@@ -76,57 +77,112 @@ SubsetMarkerModInA9 <- function (moduleEnsemblDF) {
                            , rep(7,numGenesInTreatmentGroup))))
   markersExprDF
 }
+print("#######################################################################")
 
-# Plot expression marker genes high MEF2C and low MEF2C samples
-# Separate plot for each module
-for (modToUse in modsToUse) {
-  print(modToUse)
-  markerModulesLDF <- SelectModule(modNetworkToUse, modToUse)
-  markerModulesA9LDF <- SubsetMarkerModInA9(markerModulesLDF)
-  print(head(markerModulesA9LDF))
-  # Plot expression marker genes high MEF2C and low MEF2C samples
-  quartz()
-  ggplot(markerModulesA9LDF, aes(x=hgnc_symbol, y=expression)) + 
-    geom_boxplot(aes(fill=bio.rep)) +
-    scale_fill_discrete(name= "Biological\nReplicate",
-                        labels= c("2_(high MEF2C)", "7_(low MEF2C)")) +
-    labs(title = paste(
-     "Allen derived A9 marker expression in Lipton A9\nmodule:", modToUse)) +
-    ylab("Expression (normalized FPKM)") +
-    xlab("Gene") +
-    theme_grey(base_size = 18)
-  ggsave(file= paste(
-    "../analysis/Allen A9 marker expression - minModSize 30 - module -", modToUse, Sys.Date(), ".pdf"))
-}
-
+# # Plot expression marker genes high MEF2C and low MEF2C samples
+# # Separate plot for each module
+# for (modToUse in modsToUse) {
+#   print(modToUse)
+#   markerModulesLDF <- SelectModule(modNetworkToUse, modToUse)
+#   markerModulesA9LDF <- SubsetMarkerModInA9(markerModulesLDF)
+#   print(head(markerModulesA9LDF))
+#   # Plot expression marker genes high MEF2C and low MEF2C samples
+#   quartz()
+#   ggplot(markerModulesA9LDF, aes(x=hgnc_symbol, y=expression)) + 
+#     geom_boxplot(aes(fill=bio.rep)) +
+#     scale_fill_discrete(name= "Biological\nReplicate",
+#                         labels= c("2_(high MEF2C)", "7_(low MEF2C)")) +
+#     labs(title = paste(
+#      "Allen derived A9 marker expression in Lipton A9\nmodule:", modToUse)) +
+#     ylab("Expression (normalized FPKM)") +
+#     xlab("Gene") +
+#     theme_grey(base_size = 18)
+#   ggsave(file= paste(
+#     "../analysis/Allen A9 marker expression - minModSize 30 - module -", modToUse, Sys.Date(), ".pdf"))
+# }
+# print("#######################################################################")
 
 # PCA plots
-for (modToUse in modsToUse) {
-  print(modToUse)
-  # markerModulesLDF <- SelectModule(modNetworkToUse, "sienna3")
-  markerModulesLDF <- SelectModule(modNetworkToUse, modToUse)
-  markerModulesA9LDF <- SubsetMarkerModInA9(markerModulesLDF)
-  markerModulesA9LDF <- dcast(markerModulesA9LDF, Row.names~sample, value.var = "expression")
-  row.names(markerModulesA9LDF) <- markerModulesA9LDF[ ,1]
-  markerModulesA9LDF <- markerModulesA9LDF[ ,-1]
+
+RandomModule <- function (modNetworkToUse, modSizes) {
+  blockwiseMEs <- moduleEigengenes(
+    exprData, bwModulesLL[[modNetworkToUse]]$colors)$eigengenes
+  geneModuleMembership <- as.data.frame(cor(exprData, blockwiseMEs, use = "p"))
+  moduleGenes <- c(sample(
+      1:length(bwModulesLL[[modNetworkToUse]]$colors)
+      , size = modSizes, replace=F))
+  moduleGenes <- data.frame(row.names(geneModuleMembership[moduleGenes, ]))
+  # bioMart manual:
+  # http://bioconductor.org/packages/release/bioc/vignettes/biomaRt/inst/doc/biomaRt.pdf
+  # Attribute: values to retrieve
+  # Filters: input query type
+  # Values: input query
+  ensembl <- useMart("ensembl")
+  ensembl <- useDataset("hsapiens_gene_ensembl",mart=ensembl)
+  # Data frame of module Ensembl IDs and gene symbols
+  moduleEnsemblDF <- getBM(  attributes = c("ensembl_gene_id", "hgnc_symbol")
+                             , filters = "hgnc_symbol"
+                             , values = moduleGenes
+                             , mart = ensembl)
+  moduleEnsemblDF
+}
+
+PlotMDS <- function (exprDF, module, i) {
   # mds = cmdscale(dist(markerModulesA9LDF), eig = T)
   # test <- matrix(runif(2502, 1, 10),417,6)
   # markerModulesA9LDF <- test
-  mds = cmdscale(dist(t(markerModulesA9LDF)), eig = T)
+  mds = cmdscale(dist(t(exprDF)), eig = T)
   pc1 = mds$eig[1]^2 / sum(mds$eig^2)
   pc2 = mds$eig[2]^2 / sum(mds$eig^2)
   # lapply((mds$eig^2 / sum(mds$eig^2)*100), function(x) signif(x,3))
-  
-  mdsAndTreatment <- data.frame(mds$points, as.factor(c(rep("2",3), rep("7",3))))
-  
+  mdsAndTreatment <- data.frame(mds$points
+                                , as.factor(c(rep("2",3), rep("7",3))))
+  pdf(paste("../analysis/MDS Allen A9 Marker in hESC A9 minModSize30 mod-"
+            , module, "-", i, sep = ""), height=8, width=8)
   plot(x = mdsAndTreatment[,1], y = mdsAndTreatment[,2]
        , col = as.numeric(as.factor(mdsAndTreatment[,3]))
        , pch = 16, asp=1
-       , main=paste("MDS Plot By Module Marker Gene Expression - module:", modToUse)
+       , main = paste("allen-A9-marker-expr-in-hESC-A9.R\nMDS Plot By Module"
+          , "Marker Gene Expression\nmodule:", module, sep = "")
        , xlab = paste("PC1 (", signif(100*pc1,3), "%)", sep="")
        , ylab = paste("PC2 (", signif(100*pc2,3),"%)",sep=""))
-  legend("bottomright", levels(mdsAndTreatment[,3]), col=1:length(levels(mdsAndTreatment[,3])), pch=16, cex=0.8)
+  legend("bottomright", levels(mdsAndTreatment[,3])
+         , col=1:length(levels(mdsAndTreatment[,3])), pch=16, cex=0.8)
+  dev.off()
 }
+
+# PCA for some randomly drawn modules as well as all genes in the network
+modSizes <- c(30, 30, 30, 100, 100, 100
+  , length(bwModulesLL[[modNetworkToUse]]$colors))
+i <- 0
+for (modSize in modSizes) {
+  print(modSize)
+  i <- i + 1
+  print(i)
+  randomModuleDF <- RandomModule(modNetworkToUse, modSize)
+  randomModuleDF <- SubsetMarkerModInA9(randomModuleDF)
+  randomModuleDF <- dcast(randomModuleDF, Row.names~sample
+                          , value.var = "expression")
+  # Move column 1 to row names
+  row.names(randomModuleDF) <- randomModuleDF[ ,1]
+  randomModuleDF <- randomModuleDF[ ,-1]
+  PlotMDS(randomModuleDF, modSize, i)
+}
+
+# PCA for A9 modules identified from Allen
+for (modToUse in modsToUse) {
+  i <- ""
+  print(modToUse)
+  markerModulesLDF <- SelectModule(modNetworkToUse, modToUse)
+  markerModulesA9DF <- SubsetMarkerModInA9(markerModulesLDF)
+  markerModulesA9DF <- dcast(markerModulesA9DF, Row.names~sample
+                              , value.var = "expression")
+  # Move column 1 to row names
+  row.names(markerModulesA9DF) <- markerModulesA9DF[ ,1]
+  markerModulesA9DF <- markerModulesA9DF[ ,-1]
+  PlotMDS(markerModulesA9DF, modToUse, i)
+}
+print("#######################################################################")
 
 # Mean expression
 # Loop through models of interest in network
@@ -154,23 +210,28 @@ for (modToUse in modsToUse) {
   formattedPvalggplotDF <- data.frame(
       label = paste("Paired T-test p-value:", as.character(signif(pval, 3)))
   )
-  print(ggplot(data = txMarkersMeansDF, aes(x=treatment, y=expression)) + 
-    geom_boxplot(aes(fill=treatment)) +
+  print(ggplot(data = txMarkersMeansDF, aes(x = treatment, y = expression)) + 
+    geom_boxplot(aes(fill = treatment)) +
     geom_text(data = formattedPvalggplotDF, aes(1.5, 8, label = label), type = "NA*") +
-    scale_fill_discrete(name= "Biological\nReplicate",
-                        labels= c("Tx_2_(high MEF2C)", "Tx_7_(low MEF2C)")) +
+    scale_fill_discrete(name = "Biological\nReplicate",
+                        labels = c("Tx_2_(high MEF2C)", "Tx_7_(low MEF2C)")) +
     labs(title = paste(
-      "Allen derived A9 marker expression in Lipton A9\nmodule:", modToUse)) +
+      "allen-A9-marker-expr-in-hESC-A9.R\nAllen derived A9 marker expression in"
+      , "Lipton A9\nmodule: ", modToUse, sep = "")) +
     ylab("Mean Expression (normalized FPKM)") +
     xlab("Treatment") +
     theme_bw(base_size = 18) +
-    ggsave(file= paste(
-      "../analysis/Allen A9 marker expr in hESC A9- minModSize30 - mod-", modToUse, Sys.Date(), ".pdf"))
+    ggsave(file = paste(
+      "../analysis/Allen A9 marker expr in hESC A9 minModSize30 mod-"
+      , modToUse, "-", Sys.Date(), ".pdf", sep = ""))
   )
 }
+print("#######################################################################")
 
 # Mean of expression fold changes for each module marker gene in high MEF2C
 # versus low MEF2C
+# Make list of data frames of expression (normalized FPKM) for each gene
+# Each list element is a module
 markerModulesA9LDF <- NULL
 for (modToUse in modsToUse) {
   print(modToUse)
@@ -180,72 +241,42 @@ for (modToUse in modsToUse) {
   markerModulesA9LDF[[modToUse]] <-SubsetMarkerModInA9(markerModulesLDF)
 }
 
-lapply(markerModulesA9LDF, 
+# Calculate ratio of expression in high MEF2C samples versus low MEF2C samples
+# for each gene in each module
+# List (modules) of lists (each gene in that module)
+ratioExprLL <- lapply(markerModulesA9LDF, 
   function(x) {
-  markExpr <- dcast(x, Row.names~sample, value.var="expression")
-  row.names(markExpr) <- markExpr[ ,1]
-  markExpr <- markExpr[ ,-1]
-  apply(markExpr, 1, function(x) (sum(x[1:3]) / sum(x[4:6])))
+    markExpr <- dcast(x, Row.names~sample, value.var="expression")
+    row.names(markExpr) <- markExpr[ ,1]
+    markExpr <- markExpr[ ,-1]
+    apply(markExpr, 1, function(x) (sum(x[1:3]) / sum(x[4:6])))
   }
 )
+# Mean ratio of expression in each module
+sapply(ratioExprLL, mean)
+# Paired T-test of high MEF2C versus low MEF2C expression (not ratios) for each
+# module
+sapply(markerModulesA9LDF, function(module) {
+  highMEF2C <- module[module$bio.rep=="2", ]$expression
+  lowMEF2C <-  module[module$bio.rep=="7", ]$expression
+  t.test(highMEF2C, lowMEF2C, paired = TRUE)$p.value
+})
 
-markerModulesA9LDF
-
-  # Make data frame of expression for each Tx group as col 1 and col 2
-txMarkersMeansDF <- data.frame(
-  markerModulesA9LDF[markerModulesA9LDF$bio.rep == 2, ]$expression
-  , markerModulesA9LDF[markerModulesA9LDF$bio.rep == 7, ]$expression)
-colnames(txMarkersMeansDF) <- c("Tx_2_highMEF2C", "Tx_7_lowMEF2C")
-
-foldChangesLL <- list(foldChangesLL, (txMarkersMeansDF$Tx_2_highMEF2C
-                                      / txMarkersMeansDF$Tx_7_lowMEF2C))
-
-ggplot(data = foldChangesLL, aes(x=treatment, y=expression)) + 
-        geom_boxplot(aes(fill=treatment)))
-
-
-
-print(ggplot(data = txMarkersMeansDF, aes(x=treatment, y=expression)) + 
-        geom_boxplot(aes(fill=treatment)) +
-        geom_text(data = formattedPvalggplotDF, aes(1.5, 8, label = label), type = "NA*") +
-        scale_fill_discrete(name= "Biological\nReplicate",
-                            labels= c("Tx_2_(high MEF2C)", "Tx_7_(low MEF2C)")) +
-        labs(title = paste(
-          "Allen derived A9 marker expression in Lipton A9\nmodule:", modToUse)) +
-        ylab("Mean Expression (normalized FPKM)") +
-        xlab("Treatment") +
-        theme_bw(base_size = 18) +
-        ggsave(file= paste(
-          "../analysis/Allen A9 marker expr in hESC A9- minModSize30 - mod-", modToUse, Sys.Date(), ".pdf"))
+# Boxplot of expression ratios for each module
+ratioExprDF <- melt(ratioExprLL)
+colnames(ratioExprDF) <- c("ratio.expr", "module")
+ggplot(data = ratioExprDF, aes(x=module, y=ratio.expr)) + 
+  geom_boxplot() +
+  # geom_boxplot(aes(fill=module)) +
+  labs(title = paste(
+  "allen-A9-marker-expr-in-hESC-A9.R\nAllen derived A9 marker expression in"
+  , "Lipton A9\nmodule: ", modToUse)
+  , sep = "") +
+  ylab("Mean Expression (normalized FPKM)") +
+  xlab("Treatment") +
+  theme_grey(base_size = 14) +
+  theme(axis.text = element_text(color = "black") +
+  ggsave(file = paste(
+    "../analysis/Allen A9 marker ratio expr in hESC A9 minModSize30 mod-"
+    , modToUse, "-", Sys.Date(), ".pdf", sep=""))
 )
-
-
-
-  # Paired T-test comparing high MEF2C Tx to low MEF2C Tx
-  pval <- t.test(txMarkersMeansDF$Tx_2_highMEF2C, txMarkersMeansDF$Tx_7_lowMEF2C
-                 , paired=TRUE)$p.value
-  # Format expression data for ggplot2
-  txMarkersMeansDF <- melt(txMarkersMeansDF
-                           , measure.vars=c("Tx_2_highMEF2C", "Tx_7_lowMEF2C"))
-  colnames(txMarkersMeansDF) <- c("treatment", "expression")
-  print(txMarkersMeansDF)
-  
-  # Make dataframe of pval to keep ggplot2 happy
-  formattedPvalggplotDF <- data.frame(
-    label = paste("Paired T-test p-value:", as.character(signif(pval, 3)))
-  )
-  print(ggplot(data = txMarkersMeansDF, aes(x=treatment, y=expression)) + 
-          geom_boxplot(aes(fill=treatment)) +
-          geom_text(data = formattedPvalggplotDF, aes(1.5, 8, label = label), type = "NA*") +
-          scale_fill_discrete(name= "Biological\nReplicate",
-                              labels= c("Tx_2_(high MEF2C)", "Tx_7_(low MEF2C)")) +
-          labs(title = paste(
-            "Allen derived A9 marker expression in Lipton A9\nmodule:", modToUse)) +
-          ylab("Mean Expression (normalized FPKM)") +
-          xlab("Treatment") +
-          theme_bw(base_size = 18) +
-          ggsave(file= paste(
-            "../analysis/Allen A9 marker expr in hESC A9- minModSize30 - mod-", modToUse, Sys.Date(), ".pdf"))
-  )
-}
-
