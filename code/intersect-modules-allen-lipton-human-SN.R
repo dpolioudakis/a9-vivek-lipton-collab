3c1a06f1f2ec4153a5a66ca620073b90a92755fe
Damon Polioudakis UCLA mbp <damon.polioudakis@gmail.com>
2015-08-24 16:21:59 -0700

    add save output file of module intersections

--- code/intersect-modules-allen-lipton-human-SN.R
+++ code/intersect-modules-allen-lipton-human-SN.R
@@ -7,8 +7,18 @@ library(reshape2)
 load("../Vivek_WGCNA_Lipton_A9_SN/Vivek_SN_mod_colors.rda")
 load("../processed_data/allen_BW_modules.rda")
 
+# Variable for minModSize parameter used in blockwiseModules WGCNA function
+# to record in output graph titles
+minModSize <- "100"
+
+# bwModulesLL is list of modules from different blockwiseModules parameters used
+# 11 corresponds to softPower 7, minModSize 30, deepSplit 2,
+# MEmergeCutHeight 0.25, maxBlockSize 12000
+modNetworkToUse <- 18
+
 # Split by model into lists or dataframes of genes in each model
-allenGenesModels <- split(colnames(exprData), bwModulesLL[[18]]$colors)
+allenGenesModels <- split(colnames(exprData)
+                          , bwModulesLL[[modNetworkToUse]]$colors)
 humanSNgenesModelsLL <- split(geneInfo[,1:2]
                               , geneInfo$`Initially Assigned Module Color`)
 # Dataframe of all combinations of models from Allen and Lipton human Substantia
@@ -28,7 +38,7 @@ allenHumanSNintersect <- dcast(allenHumanSNintersect
 # Filter for intersection of all genes in Allen and Lipton human substantia nigra
 totalIntersectGenesProfiled <- intersect(geneInfo$GeneSymbol
                                          , colnames(exprData))
-# Filter each model in Allen and Lipton for all genes that intersect
+# Filter each model in Allen and Lipton for all genes profiled that intersect
 allenGenesModels <- lapply(allenGenesModels, function(x)
                              intersect(x, totalIntersectGenesProfiled))
 humanSNgenesModelsLL <- lapply(humanSNgenesModelsLL, function(x)
@@ -45,10 +55,16 @@ allenHumanSNintersect <- dcast(allenHumanSNintersect
 rownames(allenHumanSNintersect) <- allenHumanSNintersect[ ,1]
 allenHumanSNintersect <- allenHumanSNintersect[ ,-1]
 # Initialize empty matrix to hold pvalues
+# Set dimension labels as module and number of genes in module
+allenModels <- split(colnames(exprData)
+                          , bwModulesLL[[modNetworkToUse]]$colors)
+allenLabels <- paste(names(allenGenesModels), sapply(allenModels, length))
+liptonLabels <- paste(names(humanSNgenesModelsLL), sapply(humanSNgenesModelsLL, length))
 pVals <- matrix( , nrow = nrow(allenHumanSNintersect)
                  , ncol = ncol(allenHumanSNintersect)
-                 , dimnames = list(rownames(allenHumanSNintersect)
-                                   , colnames(allenHumanSNintersect)))
+                 , dimnames = list(allenLabels, liptonLabels))
+paste(names(allenGenesModels), sapply(allenGenesModels, length))
+
 # Calc pbinomal of interesection for each model in Lipton human substantia nigra
 # with each model in Allen
 for (j in 1:(ncol(allenHumanSNintersect))) {
@@ -64,8 +80,8 @@ for (j in 1:(ncol(allenHumanSNintersect))) {
 # Format number of genes that intersect with respective pbinom
 textMatrix <- paste(as.matrix(allenHumanSNintersect), "\n", round(pVals,3), sep = "")
 # Plot pbinoms with number of genes that intersect included on label
-pdf(paste("../analysis/intersection_Allen_human_SN-minModSize100-", Sys.Date(), ".pdf"
-          , sep = ""), height=8, width=8)
+pdf(paste("../analysis/intersection_Allen_human_SN-minModSize30-", Sys.Date(), ".pdf"
+          , sep = ""), height=12, width=12)
 labeledHeatmap(pVals
                , yLabels = rownames(pVals)
                , xLabels = colnames(pVals)
@@ -77,3 +93,8 @@ labeledHeatmap(pVals
                , main = "pbinom Lipton Human Substantia nigra and Allen"
                )
 dev.off()
+
+dimnames(allenHumanSNintersect) <- list(allenLabels, liptonLabels)
+save(allenHumanSNintersect
+   , file = paste("../processed_data/allen_human_SN_modules_intersect_modsize"
+                  , minModSize, ".rda", sep = ""))
