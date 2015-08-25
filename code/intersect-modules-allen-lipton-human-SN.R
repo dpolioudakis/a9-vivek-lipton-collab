aae70006d8cd38f7aacf45be8b86754b71e53c01
Damon Polioudakis UCLA mbp <damon.polioudakis@gmail.com>
2015-08-24 18:03:56 -0700

    fix output graph file name

--- code/intersect-modules-allen-lipton-human-SN.R
+++ code/intersect-modules-allen-lipton-human-SN.R
@@ -9,12 +9,12 @@ load("../processed_data/allen_BW_modules.rda")
 
 # Variable for minModSize parameter used in blockwiseModules WGCNA function
 # to record in output graph titles
-minModSize <- "100"
+minModSize <- "30"
 
 # bwModulesLL is list of modules from different blockwiseModules parameters used
 # 11 corresponds to softPower 7, minModSize 30, deepSplit 2,
 # MEmergeCutHeight 0.25, maxBlockSize 12000
-modNetworkToUse <- 18
+modNetworkToUse <- 11
 
 # Split by model into lists or dataframes of genes in each model
 allenGenesModels <- split(colnames(exprData)
@@ -23,14 +23,14 @@ humanSNgenesModelsLL <- split(geneInfo[,1:2]
                               , geneInfo$`Initially Assigned Module Color`)
 # Dataframe of all combinations of models from Allen and Lipton human Substantia
 # Nigra
-modelCombos <- expand.grid(names(allenGenesModels), names(humanSNgenesModelsLL))
+moduleCombos <- expand.grid(names(allenGenesModels), names(humanSNgenesModelsLL))
 
 # Intersection genes for every combination of models
-allenHumanSNintersect <- apply(modelCombos, 1
+allenHumanSNintersect <- apply(moduleCombos, 1
   , function(x) length(intersect(allenGenesModels[[x[1]]]
                                  , humanSNgenesModelsLL[[x[2]]])))
 # Add combination of models labels
-allenHumanSNintersect <- cbind(modelCombos, allenHumanSNintersect)
+allenHumanSNintersect <- cbind(moduleCombos, allenHumanSNintersect)
 # Reshape into matrix
 allenHumanSNintersect <- dcast(allenHumanSNintersect
                                , Var1~Var2, value.var="allenHumanSNintersect")
@@ -45,9 +45,9 @@ humanSNgenesModelsLL <- lapply(humanSNgenesModelsLL, function(x)
   intersect(x$GeneSymbol, totalIntersectGenesProfiled))
 
 # Intersection genes for every combination of models
-allenHumanSNintersect <- apply(modelCombos, 1, function(x)
+allenHumanSNintersect <- apply(moduleCombos, 1, function(x)
   length(intersect(allenGenesModels[[x[1]]], humanSNgenesModelsLL[[x[2]]])))
-allenHumanSNintersect <- cbind(modelCombos, allenHumanSNintersect)
+allenHumanSNintersect <- cbind(moduleCombos, allenHumanSNintersect)
 allenHumanSNintersect <- dcast(allenHumanSNintersect
                                , Var1~Var2, value.var="allenHumanSNintersect")
 
@@ -69,10 +69,16 @@ paste(names(allenGenesModels), sapply(allenGenesModels, length))
 # with each model in Allen
 for (j in 1:(ncol(allenHumanSNintersect))) {
   for (i in 1:nrow(allenHumanSNintersect)) {
+#     pVals[i,j] <- phyper(
+#         q = (as.numeric(allenHumanSNintersect[i,j])-1)
+#       , m = length(allenGenesModels[[i]]) # genes in Allen module
+#       , n = length(totalIntersectGenesProfiled)-length(humanSNgenesModelsLL[[j]])
+#       , k = length(humanSNgenesModelsLL[[j]])
+#       , lower.tail = FALSE, log.p = FALSE)
     pVals[i,j] <- phyper(
-        q = (as.numeric(allenHumanSNintersect[i,j])-1)
+      q = (as.numeric(allenHumanSNintersect[i,j])-1)
       , m = length(allenGenesModels[[i]]) # genes in Allen module
-      , n = length(totalIntersectGenesProfiled)-length(humanSNgenesModelsLL[[j]])
+      , n = length(totalIntersectGenesProfiled)-length(allenGenesModels[[i]])
       , k = length(humanSNgenesModelsLL[[j]])
       , lower.tail = FALSE, log.p = FALSE)
   }
@@ -80,7 +86,7 @@ for (j in 1:(ncol(allenHumanSNintersect))) {
 # Format number of genes that intersect with respective pbinom
 textMatrix <- paste(as.matrix(allenHumanSNintersect), "\n", round(pVals,3), sep = "")
 # Plot pbinoms with number of genes that intersect included on label
-pdf(paste("../analysis/intersection_Allen_human_SN-minModSize30-", Sys.Date(), ".pdf"
+pdf(paste("../analysis/intersection_Allen_human_SN-minModSize", minModSize, ".pdf"
           , sep = ""), height=12, width=12)
 labeledHeatmap(pVals
                , yLabels = rownames(pVals)
@@ -94,7 +100,12 @@ labeledHeatmap(pVals
                )
 dev.off()
 
-dimnames(allenHumanSNintersect) <- list(allenLabels, liptonLabels)
-save(allenHumanSNintersect
+# List of genes that is intersection of each Allen Lipton Human SN module
+# combination.
+# List is in order of module combinations listed in moduleCombos
+intModsGenesLL <- apply(moduleCombos, 1, function(x)
+  intersect(allenGenesModels[[x[1]]], humanSNgenesModelsLL[[x[2]]]))
+
+save(intModsGenesLL, moduleCombos
    , file = paste("../processed_data/allen_human_SN_modules_intersect_modsize"
                   , minModSize, ".rda", sep = ""))
