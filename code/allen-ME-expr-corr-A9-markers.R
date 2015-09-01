14b548fa976ad694799f227592faa32696065628
Damon Polioudakis UCLA mbp <damon.polioudakis@gmail.com>
2015-09-01 11:32:33 -0700

    adjust output .pdf heatmap formating

--- code/allen-ME-expr-corr-A9-markers.R
+++ code/allen-ME-expr-corr-A9-markers.R
@@ -28,18 +28,32 @@ markerMEcor <- lapply(bwModulesLL, function(x)
   c("ALDH1A1", "TH", "SLC18A2", "CACNA1D", "CALB1"
     , "CALB2", "KCNJ6", "LMX1A", "FOXA2", "NR4A2", "ALDH1A1"), ])))
 
-pdf("../analysis/3b_MEs_markers_corr-2015-08-20.pdf", height=8, width=8)
-for (i in 1:length(blockwiseMEs)) {
+# # May need to run this code if ArgsbwModulesLLDF was not saved in
+# # allen_BW_modules.rda
+# softPower = c(5,7,9)
+# minModSize <- c(30, 50, 100)
+# deepSplit <- c(2)
+# # Cut height of 0.25, corresponds to a correlation of 0.75, to merge ME:
+# MEmergeCutHeight <- c(0.2, 0.25, 0.3)
+# maxBlockSize <- c(12000)
+# ArgsbwModulesLLDF <- expand.grid(softPower = softPower
+#                                  , minModSize = minModSize
+#                                  , deepSplit = deepSplit
+#                                  , MEmergeCutHeight = MEmergeCutHeight
+#                                  , maxBlockSize = maxBlockSize)
+
+pdf("../analysis/Allen_MEs_markers_corr.pdf", height=10, width=10)
+for (i in 1:length(bwModulesLL)) {
   labeledHeatmap(markerMEcor[[i]]
                  , xLabels = colnames(markerMEcor[[i]])
                  , yLabels = rownames(markerMEcor[[i]])
                  , ySymbols = rownames(markerMEcor[[i]])
                  , colorLabels = FALSE
-                 , colors = greenWhiteRed(50)
+                 , colors = blueWhiteRed(50)
                  , setStdMargins = TRUE
-                 , textMatrix = signif(markerMEcor[[i]],2)
-                 , cex.text = 1
-                 , cex.lab = 1
+                 , textMatrix = round(markerMEcor[[i]],2)
+                 , cex.text = 0.9
+                 , cex.lab = 0.9
                  , zlim = c(-1,1)
                  , main = paste(colnames(ArgsbwModulesLLDF)
                                 , c(ArgsbwModulesLLDF[i,]), collapse=" "))
